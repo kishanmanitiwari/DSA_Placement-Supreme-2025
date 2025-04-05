f39fd046c202fbcc21e29726b93a5b2d3517160e
Kishan Tiwari <kishanmanitiwari@gmail.com>
2025-04-05 13:35:04 +0530

    practice Questions

--- Strings/PracticeSession.java
+++ Strings/PracticeSession.java
@@ -29,7 +29,101 @@ public class PracticeSession {
 
         // return sb.toString();
 
+        return ans;
+
     }
+
+    static boolean isPalindrome(String word){
+        int start = 0 , end = word.length()-1;
+        word = word.toLowerCase();
+
+        while(start<end){
+            if (word.charAt(start)!=word.charAt(end)) {
+                return false;
+            }
+            start++; end--;
+        }
+        return true;
+    }
+
+    static boolean isVowel(char value){
+        if (value == 'a' || value == 'e' || value == 'i' || value == 'o' ||value == 'u'){
+            return true;
+        }else{
+            return false;
+        }
+     }
+
+    static int countVowel(String word){
+        // int count = 0;
+        // word = word.toLowerCase();
+
+        // for(int i=0;i<word.length();i++){
+        //     char currentChracter = word.charAt(i);
+        //     if(isVowel(currentChracter)){
+        //         count++; 
+        //     } 
+        // }
+        // return count;
+        int count = 0;
+        String vowels = "aeiouAEIOU";
+
+        for(int i=0;i<word.length();i++){
+          char currentChracter = word.charAt(i); //a
+            if(vowels.indexOf(currentChracter)!=-1){//Vowel - Count++
+                count ++;
+            }
+
+        }
+        return count;
+    }
+
+    static char firstNonRepeating(String word){
+        //frequency array
+        int[] freq = new int[26];
+
+        for(int i=0;i<word.length();i++){
+            char currentChar = word.charAt(i);
+            int insertingIndex = currentChar - 'a';
+            freq[insertingIndex]++;
+
+
+        }
+
+        System.out.println(Arrays.toString(freq));
+
+        for(int i=0;i<word.length();i++){
+            char currentChar = word.charAt(i);
+            int insertingIndex = currentChar - 'a';
+            if(freq[insertingIndex]==1){
+                //1 - non repeating
+                return currentChar;
+            }
+        }
+        return '?';
+    }
+
+    static String reverseWord(String sentence){
+        // the sky is blue - blue is sky the
+        sentence = sentence.trim();
+        String[] allWords = sentence.split("\\s+"); //[the,sky,is,blue]
+        // \\ - tab , ! , *
+        //s - spaces
+        //+ - multiple
+        System.out.println(Arrays.toString(allWords));
+        String ans = "";
+        for(int i=allWords.length-1;i>=0;i--){
+            ans=ans+allWords[i]+" ";
+            //blue is sky the 
+        }
+
+        return ans.trim();
+    }
+
+    //New Methods
+    //1) Contains - Bonus
+    //2) IndexOf
+    //4) Split ("\\s+") - split() uses regex, so some characters like ., |, *, ?, etc. need to be escaped with \\.
 
     public static void main(String[] args) {
         //Strings - Sequence of characters
@@ -87,6 +181,18 @@ public class PracticeSession {
         //10) isEmpty() - True/False
         System.out.println(myName.isEmpty()); //str.length==0
 
+        //11) IndexOf - character index - mmhb -> m index - 0 -1
+            String myNameNew = "Kisshan";
+            System.out.println(myNameNew.indexOf("ssh"));
+
+        //12)split() - dilimiter/regex = Jo input uss basisi split 
+        //eg - "Hello wolrd" - split(" ") - Array(String) = ["Hello","World"]
+        //eg - "Hello,Wolrd" - split(",") - Array(String) = ["Hello","World"]
+        //eg - "Hello,Wol-rd" - split("-") - Array(String) = ["HelloWol","rd"]
+        String exampleString = "Hello Worl-d";
+        String[] currentSplit = exampleString.split("-");
+
+        System.out.println("The length is " + Arrays.toString(currentSplit));
         // String input;
         // Scanner scn = new Scanner(System.in);
         // input = scn.next();
@@ -109,20 +215,34 @@ public class PracticeSession {
         // System.out.println(reverseString(word));
         // scn.close();
         
-        String newString = "Kishan";
-        char [] newStringChar = newString.toCharArray(); 
-        System.out.println(Arrays.toString(newStringChar));
-        newString= newString + 'D';
+        // String newString = "Kishan";
+        // char [] newStringChar = newString.toCharArray(); 
+        // System.out.println(Arrays.toString(newStringChar));
+        // newString= newString + 'D';
 
 
-        //replace
-        //kishan - kishzan
+        // //replace
+        // //kishan - kishzan
 
-        System.out.println(newString);
+        // System.out.println(newString);
 
         //11) - toCharArray()
         //12 - toString()
 
+        //Asci
+
+        // char ch = 'a'; //97
+        // char ch_1 = 'A'; //98
+
+        // int chValue = ch;
+        // int chValue_2 = ch_1;
+
+        // System.out.println(chValue);
+        // System.out.println('T'-'A');
+
+      // System.out.println(firstNonRepeating("swiss"));
+
+      System.out.println(reverseWord(" the sky                   is blue "));
 
         
     }
